70a49c2f9ce836ed496efda55f951d7a1192ce48
James Sacramento <jncsacramento@gmail.com>
2022-03-01 02:41:49 +0800

    March 1

---
+++ 4. Strings.py
@@ -0,0 +1,21 @@
+#String
+#String
+print("James Sacramento")
+#String with new line
+print("James \nSacramento")
+#String with escaped characters
+print("\"James \nSacramento\"")
+#string in a variable
+phrase = "James Sacramento is cool"
+print(phrase)
+#concatenation
+print(phrase + "cool")
+#phrase.lower
+print(phrase.lower())
+#phrase.upper
+print(phrase.upper())
+#checking upper or lower
+print(phrase.isupper())
+print(phrase.islower())
+#length
+print(len(phrase))
